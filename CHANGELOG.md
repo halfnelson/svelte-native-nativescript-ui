8d3ee58c1c1484850f67055a24764343b0bac3d0
halfnelson <dpershouse@gmail.com>
2020-03-21 11:56:15 +1000

    v0.8.1 refresh scales on child change

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.8.1] - 2020-03-21
+### Changed
+ - Call reloadScales on RadRadialGauge whenever a scale item has its configuration changed.
+
 ## [0.8.0] - 2020-03-08
 ### Changed
  - Updated to support svelte-native 0.8.0 and nativescript core modules 6.4
--- src/gauge/index.ts
+++ src/gauge/index.ts
@@ -1,6 +1,23 @@
-import { registerElement, registerNativeConfigElement, NativeElementPropType, NativeElementNode, NativeElementPropConfig } from 'svelte-native/dom'
+import { registerElement, registerNativeConfigElement, NativeElementPropType, NativeElementNode, NativeElementPropConfig, ViewNode } from 'svelte-native/dom'
 import { NativeViewElementNode } from "svelte-native/dom";
 import { RadRadialGauge, RadialScale, RadialNeedle, RadialBarIndicator, ScaleStyle, BarIndicatorStyle } from 'nativescript-ui-gauge';
+
+class RadialScaleElement extends NativeElementNode<RadialScale> {
+    onInsertedChild(childNode: ViewNode, index: number) {
+        super.onInsertedChild(childNode, index);
+        let gauge = this.parentNode as any;
+        if (gauge && gauge.nativeElement &&  gauge.nativeElement.nativeObject && gauge.nativeElement.reloadScales) {
+            gauge.nativeElement.reloadScales();
+        }
+    }
+    onRemovedChild(childNode: ViewNode) {
+        super.onRemovedChild(childNode);
+        let gauge = this.parentNode as any;
+        if (gauge && gauge.nativeElement &&  gauge.nativeElement.nativeObject && gauge.nativeElement.reloadScales) {
+            gauge.nativeElement.reloadScales();
+        }
+    }
+}
 
 
 export default class Gauges {
@@ -9,7 +26,8 @@ export default class Gauges {
             registerNativeConfigElement(tag, () => native, setsParentProp, propconfig)
             
         registerElement('radRadialGauge', () => new NativeViewElementNode<RadRadialGauge>('radRadialGauge', RadRadialGauge, null, { "scales": NativeElementPropType.ObservableArray } ));
-        registerConfigElement('radialScale', RadialScale, "scales", { "indicators": NativeElementPropType.ObservableArray })
+        registerElement('radialScale', () => new RadialScaleElement("radialScale", RadialScale, "scales",  { "indicators": NativeElementPropType.ObservableArray }));
+        //registerConfigElement('radialScale', RadialScale, "scales", { "indicators": NativeElementPropType.ObservableArray })
         registerConfigElement('radialNeedle', RadialNeedle, "indicators")
         registerConfigElement('radialBarIndicator', RadialBarIndicator, "indicators")
         registerConfigElement('scaleStyle', ScaleStyle, "scaleStyle")
